8df134b458659f9a9778f79a4be62b079846786b
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-04-03 16:09:12 -0400

    Add line numbers

--- js/editor.js
+++ js/editor.js
@@ -1,9 +1,10 @@
 import { Tab } from './tabs.js'
 import { Parser } from './parser.js'
 
-let defaultCode = "/*\n    Simple Hello World program\n    Uses the io library\n*/\n\n#include io\n\nstart:\n    lda #message\n    pha\n    jsr print\n    jmp (end)\n\n// We can use labels to point to data\nmessage:\n    .db \"Hello, world!\\n\", 0x00\n\nend:\n    jmp (end)";
+let defaultCode = "/*\n    Simple Hello World program\n    Uses the io library\n*/\n\n#include io\n\nstart:\n    lda #message\n    pha\n    jsr print\n    jmp (end)\n\n// We can use labels to point to data\nmessage:\n    .db \"Hello, world!\\n\", 0x00\n\nend:\n    jmp (end)\n";
 
 class Editor {
+    static container = document.getElementById('editor-container');
     static gutter = document.getElementById('editor-gutter');
     static input = document.getElementById('editor-input');
     static overlay = document.getElementById('editor-overlay');
@@ -17,6 +18,20 @@ class Editor {
 
     static onTextChanged() {
         this.overlay.innerHTML = Parser.tokenize(this.input.value);
+
+        let lineCount = this.input.value.split('\n').length;
+        this.gutter.innerHTML = '';
+
+        for (let i = 1; i <= lineCount; i++) {
+            const line = document.createElement('span');
+            line.innerHTML = i;
+
+            this.gutter.appendChild(line);
+        }
+    }
+
+    static setFontSize(size) {
+        this.container.style.fontSize = size + 'pt';
     }
 }
 
@@ -24,8 +39,5 @@ function onWindowLoaded() {
     Editor.newTab('Default');
 }
 
-window.onTextChanged = function() {
-    Editor.onTextChanged();
-}
-
+window.Editor = Editor;
 window.addEventListener('load', onWindowLoaded)
