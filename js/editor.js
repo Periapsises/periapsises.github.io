b69c8e81f51c16c116d686bf8f403feaea37ee1e
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-04-07 20:12:21 -0400

    Get rid of default code

--- js/editor.js
+++ js/editor.js
@@ -11,8 +11,6 @@ String.prototype.getHash = function () {
     return hash;
 };
 
-let defaultCode = "/*\n    Simple Hello World program\n    Uses the io library\n*/\n\n#include io\n\nstart:\n    lda #message\n    pha\n    jsr print\n    jmp (end)\n\n// We can use labels to point to data\nmessage:\n    .db \"Hello, world!\\n\", 0x00\n\nend:\n    jmp (end)\n";
-
 class Editor {
     static editor = document.getElementById('editor');
     static gutter = document.getElementById('gutter');
@@ -24,10 +22,6 @@ class Editor {
 
         this.input.innerHTML = code;
         this.onTextChanged();
-    }
-
-    static setDefaultCode() {
-        this.setCode(defaultCode);
     }
 
     static onTextChanged() {
@@ -57,8 +51,6 @@ class Editor {
 }
 
 function onWindowLoaded() {
-    Editor.setDefaultCode();
-
     Editor.input.addEventListener('keydown', function (e) {
         if (e.ctrlKey && e.key === 's') {
             Lua.onSave(Editor.input.value);
