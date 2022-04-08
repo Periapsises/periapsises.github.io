aa59f8fc3ca384be7845d84a4d070ad1ae16366f
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-04-07 21:08:27 -0400

    Fix GLua null check

--- js/lua.js
+++ js/lua.js
@@ -1,6 +1,6 @@
 const handler = {
     get(target, prop, receiver) {
-        if (GLua != null && prop in GLua) return GLua[prop];
+        if (typeof GLua !== 'undefined' && prop in GLua) return GLua[prop];
 
         return () => {};
     }
