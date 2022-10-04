b9cd1918e97067a06c206642e4d92526ca64d33f
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-10-04 16:29:04 -0400

    Add events and fix text color

--- mos/editor/scripts/editor.js
+++ mos/editor/scripts/editor.js
@@ -20,11 +20,27 @@ export class Editor {
         this.input = document.createElement('textarea');
         this.input.id = 'input';
         this.input.setAttribute('placeholder', 'Write your code here...')
-        this.input.setAttribute('spellcheck', 'true');
+        this.input.setAttribute('spellcheck', 'false');
+
+        this.input.addEventListener('input', () => { this.onTextChanged(this.input.value); })
+        this.input.addEventListener('keydown', (e) => {
+            if (e.ctrlKey && e.key === 's') {
+                e.preventDefault();
+            }
+
+            if (e.key === 'Tab') {
+                document.execCommand('insertText', false, '    ');
+                e.preventDefault('open_node', 'branch1');
+            }
+        })
+
         container.appendChild(this.input);
 
         this.overlay = document.createElement('pre');
         this.overlay.id = 'overlay';
         container.appendChild(this.overlay);
+    }
+
+    onTextChanged(text) {
     }
 }
