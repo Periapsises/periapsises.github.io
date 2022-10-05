f2acae8fc4444c2eea120f770d8d81b89c8b7fc5
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-10-05 18:14:27 -0400

    Fix scrolling and add line numbers

--- mos/editor/scripts/editor.js
+++ mos/editor/scripts/editor.js
@@ -4,6 +4,9 @@ export class Editor {
     input;
     overlay;
 
+    lines = [''];
+    lineNumberCount = 0;
+
     constructor() {
         this.scroller = document.createElement('div');
         this.scroller.id = 'scroller';
@@ -22,9 +25,13 @@ export class Editor {
         this.input.setAttribute('placeholder', 'Write your code here...')
         this.input.setAttribute('spellcheck', 'false');
 
-        this.input.addEventListener('input', () => { this.onTextChanged(this.input.value); })
+        this.input.addEventListener('input', () => { this.onTextChanged(); });
         this.input.addEventListener('keydown', (e) => {
             if (e.ctrlKey && e.key === 's') {
+                let event = new Event('save');
+                event.text = this.input.value;
+                this.dispatchEvent(event);
+
                 e.preventDefault();
             }
 
@@ -32,15 +39,42 @@ export class Editor {
                 document.execCommand('insertText', false, '    ');
                 e.preventDefault('open_node', 'branch1');
             }
-        })
+        });
 
         container.appendChild(this.input);
 
         this.overlay = document.createElement('pre');
         this.overlay.id = 'overlay';
         container.appendChild(this.overlay);
+
+        this.addLineNumbers();
+        this.input.focus();
     }
 
-    onTextChanged(text) {
+    onTextChanged() {
+        let inputText = this.input.value;
+
+        let cursor = this.input.selectionStart;
+        let char = inputText.charAt(cursor);
+
+        this.lines = inputText.split('\n');
+        let lineNumber = inputText.substring(0, this.input.selectionStart).split('\n').length;
+        let lineContent = this.lines[lineNumber - 1];
+
+        this.addLineNumbers();
+    }
+
+    addLineNumbers() {
+        let newCount = this.lines.length;
+        let prevCount = this.lineNumberCount;
+        if (newCount == prevCount) return;
+
+        this.gutter.innerHTML = '';
+
+        for (let line = 1; line <= newCount; line++) {
+            let span = document.createElement('span');
+            span.innerHTML = line.toString();
+            this.gutter.appendChild(span);
+        }
     }
 }
