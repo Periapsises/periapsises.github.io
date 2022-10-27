461b95721472fcc32b3469bf989ed6819d1399e8
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-10-27 16:13:18 -0400

    Add tab class

--- mos/editor/index.js
+++ mos/editor/index.js
@@ -1,3 +1,38 @@
+class Tab {
+    constructor(name) {
+        this.tab = document.createElement('div');
+        this.tab.className = 'tab';
+
+        this.icon = document.createElement('div');
+        this.icon.className = 'icon';
+        this.icon.style = 'background-image: url(\'https://raw.githubusercontent.com/PKief/vscode-material-icon-theme/83375ae073d0e21849cbc4fa583cb71d5d4358d9/icons/assembly.svg\');';
+
+        this.label = document.createElement('div');
+        this.label.className = 'label';
+        this.label.innerHTML = name || 'Untitled';
+
+        this.close = document.createElement('div');
+        this.close.className = 'close-button';
+        this.close.innerHTML = `
+            <svg width="18px" height="18px" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <use xlink:href="/fonts/VSCode-Codicons/codicon.svg#close"></use>
+            </svg>
+        `;
+
+        this.tab.appendChild(this.icon);
+        this.tab.appendChild(this.label);
+        this.tab.appendChild(this.close);
+    }
+
+    setActive(active) {
+        if (active) {
+            this.tab.classList.add('active');
+        } else {
+            this.tab.classList.remove('active');
+        }
+    }
+}
+
 class Editor {
     tabs = [];
 
@@ -5,19 +40,45 @@ class Editor {
         this.editor = document.createElement('div');
         this.editor.className = 'editor';
 
-        this.tabs = document.createElement('div');
-        this.tabs.className = 'tabs';
+        this.tabContainer = document.createElement('div');
+        this.tabContainer.className = 'tabs';
 
         this.container = document.createElement('div')
         this.container.className = 'container';
 
-        this.editor.appendChild(this.tabs);
+        this.editor.appendChild(this.tabContainer);
         this.editor.appendChild(this.container);
         document.body.appendChild(this.editor);
 
-        new Sortable(this.tabs, {
+        new Sortable(this.tabContainer, {
             animation: 350
         });
+
+        let tab = this.createTab();
+        this.selectTab(tab);
+    }
+
+    createTab() {
+        let tab = new Tab();
+        this.addTab(tab);
+
+        return tab;
+    }
+
+    addTab(tab) {
+        if (this.tabs.includes(tab)) return;
+
+        this.tabs.push(tab);
+        this.tabContainer.appendChild(tab.tab);
+    }
+
+    selectTab(tab) {
+        if (this.activeTab)
+            this.activeTab.setActive(false);
+
+        this.addTab(tab);
+        this.activeTab = tab;
+        tab.setActive(true);
     }
 }
 
