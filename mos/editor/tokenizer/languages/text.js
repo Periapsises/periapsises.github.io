e90a4ca2218c0ef56ce786df69ce545b0acf868f
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-12-01 17:42:22 -0500

    Rework editor. Again

--- mos/editor/tokenizer/languages/text.js
+++
@@ -1,8 +0,0 @@
-import { Language } from '../language.js'
-
-let states = {
-    "initial": [{ "token": "text", "regex": /^.*$/is }]
-}
-
-let TextLanguage = new Language('Text', '.txt', 'text', states);
-export { TextLanguage }
--- mos/editor/tokenizer/token.js
+++
@@ -1,6 +0,0 @@
-export class Token {
-    constructor(type, value) {
-        this.type = type;
-        this.value = value;
-    }
-}
