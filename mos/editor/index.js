cd8bb2aaf81d95b36c6da5e8f4c47ebce329f238
Periapsises <94928308+Periapsises@users.noreply.github.com>
2022-12-02 15:33:18 -0500

    More structure and text is original event value

--- mos/editor/index.js
+++ mos/editor/index.js
@@ -1,105 +1,34 @@
 import Grammar from './js/grammar.js';
 import Tokenize from './js/lexer.js';
-
-/**
- * Handles tokenizing the new text and adjusting the gutter line numbers
- * @param {string} text 
- * @param {HTMLDivElement} overlay 
- * @param {HTMLDivElement} gutter
- */
-const onTextinputChanged = function (text, overlay, gutter) {
-    overlay.innerHTML = '';
-    gutter.innerHTML = '';
-
-    let lineCount = text.split('\n').length;
-    for (let i = 0; i < lineCount; i++) {
-        let lineNum = document.createElement('span');
-        lineNum.innerHTML = (i + 1).toString();
-        gutter.appendChild(lineNum);
-    }
-
-    let tokens = Tokenize(text, Grammar);
-    tokens.forEach(token => {
-        let element = document.createElement('span');
-        element.innerHTML = token.value;
-        element.classList = 'token ' + token.type;
-
-        overlay.appendChild(element);
-    });
-}
-
-/**
- * Handle key shortcuts
- * @param {KeyboardEvent} e The event that was triggered
- */
-const onKeyDownEvent = function (e) {
-    let isCtrl = e.ctrlKey;
-    let key = e.key;
-
-    // Ctrl+S
-    if (isCtrl && key === 's') {
-        e.preventDefault();
-
-        let event = new Event('editorsave');
-        event.text = Editor.input.value;
-        document.dispatchEvent(event);
-    }
-
-    // Tab
-    if (!isCtrl && key === 'Tab') {
-        e.preventDefault();
-        document.execCommand('insertText', false, '    ');
-        Editor.input.dispatchEvent(new Event('input'));
-    }
-}
-
-/**
- * Creates the required HTML elements to form an Editor
- */
-const createEditorHTML = function () {
-    let editor = document.createElement('div');
-    editor.className = 'editor';
-
-    let container = document.createElement('div');
-    container.className = 'container';
-
-    let gutter = document.createElement('div');
-    gutter.className = 'gutter';
-
-    let input = document.createElement('textarea');
-    input.className = 'input';
-    input.placeholder = 'Enter your code here';
-    input.spellcheck = false;
-
-    let overlay = document.createElement('div');
-    overlay.className = 'overlay';
-
-    input.addEventListener('input', () => onTextinputChanged(input.value, overlay, gutter));
-    input.addEventListener('keydown', onKeyDownEvent)
-
-    let e = new Event('input');
-    input.dispatchEvent(e);
-
-    container.appendChild(input);
-    container.appendChild(overlay);
-    editor.appendChild(gutter);
-    editor.appendChild(container);
-    document.body.appendChild(editor);
-
-    window.Editor.input = input;
-}
+import Editor from './js/editor.js';
 
 function onContentLoaded() {
-    let Editor = {
-        setCode: text => {
-            Editor.input.value = text;
-            let e = new Event('input');
-            Editor.input.dispatchEvent(e);
-        }
-    };
+    window.Editor = new Editor();
+    let gutter = window.Editor.gutter;
+    let overlay = window.Editor.overlay;
 
-    window.Editor = Editor;
-    createEditorHTML();
+    document.addEventListener('editortextchanged', (e) => {
+        let text = e.text;
+
+        gutter.innerHTML = '';
+        overlay.innerHTML = '';
+
+        let lineCount = text.split('\n').length;
+        for (let i = 0; i < lineCount; i++) {
+            let lineNum = document.createElement('span');
+            lineNum.innerHTML = (i + 1).toString();
+            gutter.appendChild(lineNum);
+        }
+
+        let tokens = Tokenize(text, Grammar);
+        tokens.forEach(token => {
+            let element = document.createElement('span');
+            element.innerHTML = token.value;
+            element.classList = 'token ' + token.type;
+
+            overlay.appendChild(element);
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', onContentLoaded);
---
+++ mos/editor/js/editor.js
@@ -0,0 +1,98 @@
+String.prototype.getHash = function () {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+export default class Editor {
+    constructor() {
+        this.#createEditorHTML();
+        this.input.addEventListener('input', (e) => this.#onTextChanged(e));
+        this.input.addEventListener('keydown', (e) => this.#onKeyBind(e));
+    }
+
+    setHash(hash) {
+        this.hash = hash;
+    }
+
+    /**
+     * Generates the required HTML elements to display the editor
+     */
+    #createEditorHTML() {
+        let editor = document.createElement('div');
+        editor.classList.add('editor');
+
+        let container = document.createElement('div');
+        container.classList.add('container');
+
+        let gutter = document.createElement('div');
+        gutter.classList.add('gutter');
+
+        let input = document.createElement('textarea');
+        input.classList.add('input');
+        input.placeholder = 'Enter your code here.';
+        input.spellcheck = false;
+
+        let overlay = document.createElement('div');
+        overlay.classList.add('overlay');
+
+        container.appendChild(input);
+        container.appendChild(overlay);
+        editor.appendChild(gutter);
+        editor.appendChild(container);
+        document.body.appendChild(editor);
+
+        this.editor = editor;
+        this.container = container;
+        this.gutter = gutter;
+        this.input = input;
+        this.overlay = overlay;
+    }
+
+    /**
+     * Called when the contents of the textarea changes
+     * @param {KeyboardEvent} e The event that was fired
+     */
+    #onTextChanged(e) {
+        let text = this.input.value;
+        let isOriginal = this.hash == text.getHash();
+
+        let textChangedEvent = new Event('editortextchanged');
+        textChangedEvent.editor = this;
+        textChangedEvent.text = text;
+        textChangedEvent.isOriginal = isOriginal
+        document.dispatchEvent(textChangedEvent);
+    }
+
+    /**
+     * Runs when a key is pressed on the input of the editor
+     * @param {KeyboardEvent} e The event that was fired
+     */
+    #onKeyBind(e) {
+        let ctrl = e.ctrlKey;
+        let key = e.key;
+
+        // Ctrl+S
+        if (ctrl && key === 's') {
+            let saveEvent = new Event('editorsave');
+            saveEvent.editor = this;
+            document.dispatchEvent(saveEvent);
+
+            e.preventDefault();
+        }
+
+        // Tab
+        if (!ctrl && key === 'Tab') {
+            document.execCommand('insertText', false, '    ');
+            let changeEvent = new Event('input');
+            this.input.dispatchEvent(changeEvent);
+
+            e.preventDefault();
+        }
+    }
+}
